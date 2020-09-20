679f873fe787c83be5b538a95a10e67a9c43fc62
Azat Gilyazov <azatpscl@gmail.com>
2020-09-20 12:57:10 +0300

    Add name editing feature

--- app/src/main/java/com/example/anteeoneapp/MainActivity.kt
+++ app/src/main/java/com/example/anteeoneapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anteeoneapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,7 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         id_editNameButton.setOnClickListener {
-            
+
+            if(id_name.visibility == View.VISIBLE){
+                id_name.setVisibility(View.INVISIBLE)
+                id_editNameText.setVisibility(View.VISIBLE)
+                id_editNameText.setText(id_name.text)
+            }
+            else{
+                id_name.setVisibility(View.VISIBLE)
+                id_editNameText.setVisibility(View.INVISIBLE)
+                id_name.text = id_editNameText.text
+            }
+
         }
+        
     }
 }
