8af0fccb8f2cd980540471178d180fae0253d8ed
Azat Gilyazov <azatpscl@gmail.com>
2020-10-15 11:58:09 +0300

    Add fragments hw

--- app/src/main/java/com/example/anteeoneapp/MainActivity.kt
+++ app/src/main/java/com/example/anteeoneapp/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.anteeoneapp
 
+
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        id_bottomNav_1.setOnClickListener {
+            supportFragmentManager.beginTransaction().
+                    replace(R.id.id_frameLayout,Fragment1.newInstance("1","1")).commit()
+            configButtonColors()
+            id_bottomNav_1.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent))
+
+        }
+        id_bottomNav_2.setOnClickListener {
+            supportFragmentManager.beginTransaction().
+                    replace(R.id.id_frameLayout,Fragment2.newInstance("1","1")).commit()
+            configButtonColors()
+            id_bottomNav_2.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent))
+        }
+        id_bottomNav_3.setOnClickListener {
+            supportFragmentManager.beginTransaction().
+                    replace(R.id.id_frameLayout,Fragment3.newInstance("1","1")).commit()
+            configButtonColors()
+            id_bottomNav_3.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent))
+        }
+        id_bottomNav_4.setOnClickListener {
+            supportFragmentManager.beginTransaction().
+                    replace(R.id.id_frameLayout,Fragment4.newInstance("1","1")).commit()
+            configButtonColors()
+            id_bottomNav_4.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent))
+        }
+        id_bottomNav_5.setOnClickListener {
+            supportFragmentManager.beginTransaction().
+                    replace(R.id.id_frameLayout,Fragment5.newInstance("1","1")).commit()
+            configButtonColors()
+            id_bottomNav_5.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent))
+        }
+
+    }
+
+
+    fun configButtonColors(){
+        id_bottomNav_1.setColorFilter(ContextCompat.getColor(this,R.color.black))
+        id_bottomNav_2.setColorFilter(ContextCompat.getColor(this,R.color.black))
+        id_bottomNav_3.setColorFilter(ContextCompat.getColor(this,R.color.black))
+        id_bottomNav_4.setColorFilter(ContextCompat.getColor(this,R.color.black))
+        id_bottomNav_5.setColorFilter(ContextCompat.getColor(this,R.color.black))
     }
 }
+
